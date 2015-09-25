7ad708f9d8edd1b063a860dc4430d629b924110f
jiayingy <jiayinglek@gmail.com>
2015-09-25 15:04:30 +0800

    edited Parser

--- TaskBuddy/src/Parser.java
+++ TaskBuddy/src/Parser.java
@@ -1,54 +1,58 @@
+import java.util.*;
 
 public class Parser {
 	
+	
+	final String ERROR_COMMAND = "Invalid User Command.";
 	String userInput;
-	
-	enum COMMAND_TYPE {
-		ADD, DISPLAY, EDIT, DISPLAY_SPECIFIC, HELP, UNDO, DELETE, SEARCH, CLEAR, EXIT, INVALID
+
+	public Parser() {		
 	}
 	
-	public Parser(String input) {
-		userInput = input.trim();
-	}
-	
-	COMMAND_TYPE getCommand () {
-		if (userInput.contains("add")) {
-			return COMMAND_TYPE.ADD;
-		} else if (userInput.contains("delete")) {
-			return COMMAND_TYPE.DELETE;
-		} else if (userInput.contains("display on")) {
-			return COMMAND_TYPE.DISPLAY_SPECIFIC;
-		} else if (userInput.contains("display")) {
-			return COMMAND_TYPE.DISPLAY;
-		} else if (userInput.contains("edit")) {
-			return COMMAND_TYPE.EDIT;
-		} else if (userInput.contains("search")) {
-			return COMMAND_TYPE.SEARCH;
-		} else if (userInput.contains("undo")) {
-			return COMMAND_TYPE.UNDO;
-		} else if (userInput.contains("help")) {
-			return COMMAND_TYPE.HELP;
-		} 
-		return COMMAND_TYPE.INVALID;
-	} 
-	
-	String[] getSortedContent(COMMAND_TYPE command) {	
-		userInput.replace(command.toString(),"");
-		String[] sortedInput = sortContents(command,userInput);
-		return sortedInput;
-	}
-	
-	String[] sortContents(COMMAND_TYPE command, String contents) {
-		// sort
-		// format date formatDate(date);
+	Map<String, String> getDictionary (String input) {
+		Map<String, String> dictionary = new HashMap<String,String>();
 		
-		String[] arr = contents.split(" ");
-		
-		return arr;
-	}
-	
-	String formatDate(String date) {
-		return date;
+		userInput = input;
+		String command = extractCommand(dictionary);
+		extractContent(command, dictionary);
+		return dictionary;
 	}
 
+	private String extractCommand(Map<String,String> dictionary) {
+		String command="";
+		if (userInput.contains("add")) {
+			command = "add";
+			dictionary.put("command", "add");
+		} else if (userInput.contains("delete")) {
+			command = "delete";
+			dictionary.put("command", "delete");
+		} else if (userInput.contains("display on")) {
+			command = "display on";
+			dictionary.put("command", "display on");
+		} else if (userInput.contains("display")) {
+			command = "display";
+			dictionary.put("command", "display");
+		} else if (userInput.contains("edit")) {
+			command = "edit";
+			dictionary.put("command", "edit");
+		} else if (userInput.contains("search")) {
+			command = "search";
+			dictionary.put("command", "search");
+		} else if (userInput.contains("undo")) {
+			command = "undo";
+			dictionary.put("command", "undo");
+		} else if (userInput.contains("help")) {
+			command = "help";
+			dictionary.put("command", "help");
+		} else {
+			System.out.println(ERROR_COMMAND);
+		}
+		return command;
+	} 
+	
+	private void extractContent(String command, Map<String,String> dictionary) {
+		// code
+		if (command.equals("add")) {
+		}
+	}
 }
