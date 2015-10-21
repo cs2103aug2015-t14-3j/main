5f06a4d790f4ea7629ce28be24f0a961f4b6c7bc
Derek Wang <derekxinzhewang@gmail.com>
2015-10-21 21:03:49 +0800

    DateParser -> remove comment

--- src/main/java/com/cs2013t143j/TaskBuddyM/Parser/DateParser.java
+++ src/main/java/com/cs2013t143j/TaskBuddyM/Parser/DateParser.java
@@ -49,11 +49,11 @@ public class DateParser {
 		Map<String, String> localizedParseResult = matchDateByLocalFormat(description);
 		switch (localizedParseResult.size()) {
 			case 0:
-				System.out.println("parse1");
+				//System.out.println("parse1");
 				parseDateByNatty(0);
 				break;
 			case 1:
-				System.out.println("parse2");
+				//System.out.println("parse2");
 				String dateString = null;
 				for (String dateFormat : localizedParseResult.keySet()) {
 					dateString = localizedParseResult.get(dateFormat);
@@ -62,7 +62,7 @@ public class DateParser {
 				parseDateByNatty(1);
 				break;
 			case 2:
-				System.out.println("parse3");
+				//System.out.println("parse3");
 				// set by order, first is startDate second is endDate
 				for (String dateFormat : localizedParseResult.keySet()) {
 					if(resDict.get(STARTDATE) == null){
@@ -109,7 +109,7 @@ public class DateParser {
 	            Matcher m = p.matcher(dateString);
 	            while(m.find()) {
 	            	String matchDateString = m.group();
-	            	System.out.println(matchDateString);
+	            	//System.out.println(matchDateString);
 	            	SimpleDateFormat simpleDateFormat = new SimpleDateFormat(format);
 	            	try {
 						Date matchDate = simpleDateFormat.parse(matchDateString);
@@ -123,7 +123,7 @@ public class DateParser {
 	            }
 	        }
 	    }
-	    System.out.println("res size is "+ res.size());
+	    //System.out.println("res size is "+ res.size());
 	    return res; 
 	}
 	
@@ -144,10 +144,10 @@ public class DateParser {
 	private void determineLocal(String input, String dateString) {
 		for (String regexp : TIME_KEY_REGEXPS.keySet()) {
 	    	// this check may be redundant 
-			System.out.println("Somet1");
-			System.out.println(input.toLowerCase());
+			//System.out.println("Somet1");
+			//System.out.println(input.toLowerCase());
 	        if (input.toLowerCase().matches("^.*"+regexp+".*$")) {
-	        	System.out.println("Somet2");
+	        	//System.out.println("Somet2");
 	        	boolean d = TIME_KEY_REGEXPS.get(regexp);
 	        	if(d){
 	        		resDict.put(STARTDATE, dateString);
