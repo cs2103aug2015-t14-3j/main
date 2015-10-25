011510155ec85f368a5f636a4c2ac45d2b51e543
Daniel <danielchow798@gmail.com>
2015-10-25 13:05:28 +0800

    Added Command to sort last display by start date

---
+++ src/main/java/com/cs2013t143j/TaskBuddyM/Command/SortDisplay.java
@@ -0,0 +1,61 @@
+package com.cs2013t143j.TaskBuddyM.Command;
+
+import java.util.ArrayList;
+import java.util.Collections;
+
+import com.cs2013t143j.TaskBuddyM.Logic.StorageAccess;
+import com.cs2013t143j.TaskBuddyM.Storage.Task;
+
+
+public class SortDisplay implements Command {
+	
+	protected final String DISPLAY_FORMAT = "%d.%s\t%s\t%s\t%s\n";
+	private final String SORTED_HEADER = "Here are your tasks sorted by start date\n";
+	protected final String FREE_DAY = "Looks like there is nothing on your schedule. Enjoy your day!!!\n\n";
+
+	public String execute(ArrayList<Task> lastDisplay, StorageAccess sAccess) {
+		
+		String output = SORTED_HEADER;
+		
+		if (lastDisplay.size() == 0) {
+			output = FREE_DAY;
+			return output;
+		}
+		
+		Collections.sort(lastDisplay, new StartSorter());
+	
+		int index = 1;
+		
+		int i = 0;
+		
+		for (i = 0; i < lastDisplay.size(); ++i) {
+			Task task = lastDisplay.get(i);
+			
+			String description = task.getDescription();
+			String start = task.getStartDateTimeInString();
+			String end = task.getEndDateTimeInString();
+			
+			if (start == "") {
+				start = "-";
+			}
+			
+			if (end == "") {
+				end = "-";
+			}
+			
+			boolean done = task.isDone();
+			String isDone = "No";
+			
+			if (done == true) {
+				isDone = "Yes";
+			}else {
+				isDone = "No";
+			}
+			
+			output += String.format(DISPLAY_FORMAT, index, description, start, end, isDone);			
+			++index;
+		}
+		
+		return output;
+	}
+}
