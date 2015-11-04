465914b2141f42654b653e8917e2d1e1f000569f
Daniel <danielchow798@gmail.com>
2015-11-04 21:28:40 +0800

    Added support for more date formats for display range

--- src/main/java/com/cs2013t143j/TaskBuddyM/Command/DisplayRange.java
+++ src/main/java/com/cs2013t143j/TaskBuddyM/Command/DisplayRange.java
@@ -2,6 +2,7 @@ package com.cs2013t143j.TaskBuddyM.Command;
 
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
+import java.time.format.DateTimeParseException;
 import java.util.ArrayList;
 
 import com.cs2013t143j.TaskBuddyM.Logic.StorageAccess;
@@ -16,77 +17,112 @@ public class DisplayRange extends DisplayCommand {
 	
 	private final String INFO = "Display Range %s %s";
 	
-	private final String DATE_FORMAT = "HH dd/MM/yyyy";	
+	private final String ERROR_FORMAT = "Invalid date format(Should be dd/mm/yyyy)";
+
+	private final int DAY_POS = 0;
+	private final int MONTH_POS = 1;
+	private final int YEAR_POS = 2;
 	
 	public DisplayRange(String _startDate, String _endDate) {
 		startDate = _startDate;
 		endDate = _endDate;
 	}
-	
+
 	public String execute(ArrayList<Task> lastDisplay, StorageAccess sAccess) throws CommandAttributeError {
-		
+
 		if (startDate == null || startDate == "" || startDate == " ") {
 			throw new CommandAttributeError(ERROR_DATE);
 		} else if (endDate == null || endDate == "" || endDate == " ") {
 			throw new CommandAttributeError(ERROR_DATE);
 		}
-		
+
 		tasks = extractWithin(startDate, endDate, sAccess);
-		
+
 		output = parseTasks(output);
 
 		return output;
 	}
-private ArrayList<Task> extractWithin(String startDate,String endDate,StorageAccess sAccess){
+	
+	private ArrayList<Task> extractWithin(String startDate,String endDate,StorageAccess sAccess) throws CommandAttributeError {
 		ArrayList<Task> TaskList = new ArrayList<Task>();
-		
+
 		LocalDateTime startDateTime = convertDateTime(startDate);
 		LocalDateTime endDateTime = convertDateTime(endDate);
-		
-		TaskList = sAccess.searchPeriod(startDateTime, endDateTime);
-		
-		return TaskList;
-}
 
-private LocalDateTime convertDateTime(String dateTime) {
-	
-	if (dateTime == null) {
-		return null;
+		TaskList = sAccess.searchPeriod(startDateTime, endDateTime);
+
+		return TaskList;
 	}
-	String[] splitDateTime = dateTime.split(" ");
-	DateTimeFormatter formatter;
-	LocalDateTime dt;
-	
-	if (splitDateTime.length == 1) {
-		//No time specified
-		try {
-			formatter = DateTimeFormatter.ofPattern(DATE_FORMAT);
-			dt = LocalDateTime.parse("2359 " + dateTime, formatter);
-		} catch (Exception e) {
-			formatter = DateTimeFormatter.ofPattern(DATE_FORMAT);
-			dt = LocalDateTime.parse("2359 " + dateTime, formatter);
+
+	private LocalDateTime convertDateTime(String dateTime) throws CommandAttributeError {
+
+		if (dateTime == null) {
+			return null;
 		}
-		
-		
-		return dt;
-	} else {
-		//Time specified
-		try {
-			formatter = DateTimeFormatter.ofPattern(DATE_FORMAT);
-			dt = LocalDateTime.parse(dateTime, formatter);
-		} catch (Exception e) {
-			formatter = DateTimeFormatter.ofPattern(DATE_FORMAT);
-			dt = LocalDateTime.parse(dateTime, formatter);
+		DateTimeFormatter formatter;
+		LocalDateTime dt;
+
+		String format = createFormatter(dateTime);
+		formatter = DateTimeFormatter.ofPattern(format);
+
+		int dateTimeSize = dateTime.split(" ").length;
+
+		try{
+			if (dateTimeSize == 2) {
+				dt = LocalDateTime.parse(dateTime, formatter);
+			} else {
+				dt = LocalDateTime.parse("23 " + dateTime, formatter);
+			}
+		} catch (DateTimeParseException e) {
+			throw new CommandAttributeError(ERROR_FORMAT);
 		}
-		
-		
 		return dt;
 	}
-}
-	
-public String info() {
+
+	private String createFormatter(String dateTime) throws CommandAttributeError {
+		//Generates a format string for DateTimeFormatter based on the input datetime string
+
+		String format = "HH ";
+		String date;
+
+		String[] splitDT = dateTime.split(" ");
+
+		if (splitDT.length == 1) {
+			//No Time specified
+			date = splitDT[0];
+		} else {
+			// Time specified
+			date = splitDT[1];
+		}
+
+		String[] splitDate = date.split("/");
+
+		int daySize = splitDate[DAY_POS].length();
+		int monthSize = splitDate[MONTH_POS].length();
+		int yearSize = splitDate[YEAR_POS].length();
+
+		int i =0 ;
+
+		for (i=0; i<daySize; ++i) {
+			format += "d";
+		}
+		format += "/";
+
+		for (i=0; i<monthSize; ++i) {
+			format += "M";
+		}
+		format += "/";
+
+		for (i=0; i<yearSize; ++i) {
+			format += "y";
+		}
+
+		return format;
+	}
+
+	public String info() {
 		String output = String.format(INFO, startDate, endDate);
-		
+
 		return output;
 	}
 }
