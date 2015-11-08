420c6946e9a4197b1b1b5ea3012b1dbee79ce05a
gokulr914 <gokulr914@gmail.com>
2015-11-08 12:08:06 +0800

    display month

---
+++ src/main/java/com/cs2013t143j/TaskBuddyM/Command/DisplayMonth.java
@@ -0,0 +1,34 @@
+package com.cs2013t143j.TaskBuddyM.Command;
+
+import java.util.ArrayList;
+
+import com.cs2013t143j.TaskBuddyM.Logic.StorageAccess;
+import com.cs2013t143j.TaskBuddyM.Storage.Task;
+
+public class DisplayMonth  extends DisplayCommand{
+
+private final String DISPLAY_HEADER_MONTH = "Here are all your tasks for the month:\n";
+	
+private final String INFO = "Display Month";
+	
+	
+public String execute(ArrayList<Task> lastDisplay, StorageAccess sAccess) {
+		
+		ArrayList<Task> allTasks = sAccess.showMonth();
+		
+		tasks = allTasks;
+		
+		String output = DISPLAY_HEADER_MONTH;
+		
+		output = parseTasks(output);
+
+		return output;
+	}
+public String info() {
+	String output = INFO;
+	
+	return output;
+}	
+	
+	
+}
